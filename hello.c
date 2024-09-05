5b73d5b73d87347550986a8cc90698fe45abebe5
Laura Andrade <devandrrade@gmail.com>
2024-09-05 19:10:47 +0000

    Thu, Sep 5, 2024, 4:10 PM -03:00

--- hello.c
+++ hello.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 
 int main(void){
-    string answer = get_string("What's your name? ");
-    printf("Hello, %s\n", answer);
+    string name = get_string("What's your name? ");
+    printf("Hello, %s\n", name);
 }
