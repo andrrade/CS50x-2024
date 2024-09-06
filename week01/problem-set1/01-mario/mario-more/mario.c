51e1a649992a64ac38dc749f6b62d9133f07d244
Laura Andrade <devandrrade@gmail.com>
2024-09-06 18:49:58 +0000

    Fri, Sep 6, 2024, 3:49 PM -03:00

--- week01/problem-set1/01-mario/mario-more/mario.c
+++ week01/problem-set1/01-mario/mario-more/mario.c
@@ -10,18 +10,15 @@ int main(void){
     while(n < 1 || n > 8);
 
     for(int i = 0; i < n; i++){
+
         for(int j = 0; j < n - i - 1; j++){
+
             printf(" ");
         }
 
 
-        for(int j = 0; j <= n*2 +2; j++){
-            if(j == n - 1){
-                printf(" ");
-            }
-            else{
-                printf("#");
-            }
+        for(int j <= n*2 +2; j <= i ; j++){
+            printf(" ");
         }
 
 
