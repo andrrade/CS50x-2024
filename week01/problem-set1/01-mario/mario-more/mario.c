bf070bb7ce2cbab9f32ecadf313dd6321ea69747
Laura Andrade <devandrrade@gmail.com>
2024-09-06 18:51:49 +0000

    Fri, Sep 6, 2024, 3:51 PM -03:00

--- week01/problem-set1/01-mario/mario-more/mario.c
+++ week01/problem-set1/01-mario/mario-more/mario.c
@@ -1,28 +1,20 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void){
     int n;
-
-    do{
+    do {
         n = get_int("Height: ");
     }
-    while(n < 1 || n > 8);
+    while (n < 1 || n > 8);
 
-    for(int i = 0; i < n; i++){
-
-        for(int j = 0; j < n - i - 1; j++){
-
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
             printf(" ");
         }
-
-
-        for(int j <= n*2 +2; j <= i ; j++){
-            printf(" ");
+        for (int j = 0; j <= i; j++) {
+            printf("#");
         }
-
-
         printf("\n");
     }
-
 }
