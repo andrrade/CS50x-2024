34001d1cce351099f7c597ff1236b5c808b851ac
Laura Andrade <devandrrade@gmail.com>
2024-09-06 18:37:19 +0000

    Fri, Sep 6, 2024, 3:37 PM -03:00

--- week01/problem-set1/01-mario/mario-more/mario.c
+++ week01/problem-set1/01-mario/mario-more/mario.c
@@ -8,4 +8,16 @@ int main(void){
         n = get_int("Height: ");
     }
     while(n < 1 || n > 8);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            printf(" ");
+        }
+
+        for(int j = 0; j <= i; j++){
+            printf("#");
+        }
+        printf("\n");
+    }
+
 }
