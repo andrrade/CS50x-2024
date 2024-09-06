5f2e6caf3132f82c3f7f81373753bafae2bc6a1b
Laura Andrade <devandrrade@gmail.com>
2024-09-06 12:58:22 +0000

    Fri, Sep 6, 2024, 9:58 AM -03:00

--- mario.c
+++ mario.c
@@ -2,8 +2,12 @@
 #include <cs50.h>
 
 int main(void){
-    for(int i = 0; i < 4; i++){
-        printf("?");
+    const int n = get_int("Size: ");
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("#");
+        }
+        printf("\n");
     }
-    printf("\n");
 }
