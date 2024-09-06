5f5cdb15dae957be0720b7bf7931a613a1d468ed
Laura Andrade <devandrrade@gmail.com>
2024-09-06 12:18:36 +0000

    Fri, Sep 6, 2024, 9:18 AM -03:00

--- 07-calculator.c
+++ 07-calculator.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int add(void);
+int add(int a, int b);
 
 int main(void){
     int x = get_int("x: ");
     int y = get_int("y: ");
-    int z = add();
-    
+    int z = add(x, y);
+
     printf("%i\n", z);
 }
 
-int add(void){
-    return x + y;
+int add(int a, int b){
+    return a + b;
 }
