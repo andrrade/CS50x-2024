993005bf56a4aa8a3a0ed06fd5099586af2a89ee
Laura Andrade <devandrrade@gmail.com>
2024-09-06 18:52:52 +0000

    Fri, Sep 6, 2024, 3:52 PM -03:00

--- week01/problem-set1/01-mario/mario-more/mario.c
+++ week01/problem-set1/01-mario/mario-more/mario.c
@@ -17,4 +17,12 @@ int main(void){
         }
         printf("\n");
     }
+
+    printf(" ");
+     for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
