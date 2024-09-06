0b2a040b43f132762a1b2fdcfa269a98bdd54cd7
Laura Andrade <devandrrade@gmail.com>
2024-09-06 12:02:32 +0000

    Fri, Sep 6, 2024, 9:02 AM -03:00

--- 06-for.c
+++ 06-for.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void meow(void){
-    printf("meow\n");
-}
+void meow(void);
 
 int main(void){
     for(int i = 0; i < 3; i++){
         meow();
     }
+}
+
+void meow(void){
+    printf("meow\n");
 }
