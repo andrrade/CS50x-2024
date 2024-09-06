f39d445be6170261e82188b188201a67c4d2b2f9
Laura Andrade <devandrrade@gmail.com>
2024-09-06 18:21:44 +0000

    Fri, Sep 6, 2024, 3:21 PM -03:00

--- week01/problem-set1/01-mario/mario-less/mario.c
+++ week01/problem-set1/01-mario/mario-less/mario.c
@@ -8,12 +8,10 @@ int main(void){
     }
     while (n < 1 || n > 8);
 
-    for (int i = 0; i < n; i++) { // rows
-
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            printf(" "); // print spaces (invisible dots)
+            printf(" ");
         }
-        // Imprime hashes
         for (int j = 0; j <= i; j++) {
             printf("#");
         }
