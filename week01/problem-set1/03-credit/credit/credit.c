044742f56324881fb65a45b78ca9c4c7460ae9e3
Laura Andrade <devandrrade@gmail.com>
2024-09-10 12:40:18 +0000

    Tue, Sep 10, 2024, 9:40 AM -03:00

---
+++ week01/problem-set1/03-credit/credit/credit.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    do{
+        long number = get_long("Number: ");
+    } while (number <= 0);
+
+    if(number < 9999999999999){
+        printf("INVALID");
+    }
+    else{
+        if(number >)
+    }
+
+
+}
