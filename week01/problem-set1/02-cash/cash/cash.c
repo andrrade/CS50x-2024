21a7720df9ed35a2934b8f845a4dd31661eb33b0
Laura Andrade <devandrrade@gmail.com>
2024-09-10 11:15:42 +0000

    Tue, Sep 10, 2024, 8:15 AM -03:00

--- week01/problem-set1/02-cash/cash/cash.c
+++ week01/problem-set1/02-cash/cash/cash.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <math.h>
 
 int main(void){
     float change;
@@ -10,21 +9,12 @@ int main(void){
     }
     while(change < 0);
 
-    int cents = round(change * 100);
-
-    int quarters = cents / 25;
-    cents %= 25;
-
-    int dimes = cents / 10;
-    cents %= 10;
-
-    int nickles = cents / 5;
-    cents %= 5;
-
-    int pennies = cents / 1;
-    cents %= 1;
 
     int total_coins = quarters + dimes + nickles + pennies;
 
-    printf("Total coins: %d\n", total_coins);
+    printf(total_coins);
+}
+
+int calculate_coins(){
+    
 }
