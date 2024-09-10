66a8c06c56f1fdff764acb6b0236abe6b6a34c96
Laura Andrade <devandrrade@gmail.com>
2024-09-10 11:11:00 +0000

    Tue, Sep 10, 2024, 8:11 AM -03:00

--- week01/problem-set1/02-cash/cash/cash.c
+++ week01/problem-set1/02-cash/cash/cash.c
@@ -13,16 +13,16 @@ int main(void){
     int cents = round(change * 100);
 
     int quarters = cents / 25;
-    cents =% 25;
+    cents %= 25;
 
     int dimes = cents / 10;
-    cents =% 10;
+    cents %= 10;
 
     int nickles = nickles / 5;
-    cents =% 5;
+    cents %= 5;
 
     int pennies = cents / 1;
-    cents =% 1;
+    cents %= 1;
 
     int total_coins = quarters + dimes + nickles + pennies;
 
