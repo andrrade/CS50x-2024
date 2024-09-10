6ea0d316eab84f3695d00cfd406097efc2cb1c75
Laura Andrade <devandrrade@gmail.com>
2024-09-10 12:51:32 +0000

    Tue, Sep 10, 2024, 9:51 AM -03:00

--- week01/problem-set1/03-credit/credit/credit.c
+++ week01/problem-set1/03-credit/credit/credit.c
@@ -18,11 +18,11 @@ int main(void) {
     // Verifica o comprimento e o prefixo
     if (length < 13 || length > 16) {
         printf("INVALID\n");
-    } else if ((length >= 13 && length <= 16) && (number / 1000000000000 == 34 || number / 1000000000000 == 37)) {
+    } else if ((length == 15) && (number / 1000000000000 == 34 || number / 1000000000000 == 37)) {
         printf("AMEX\n");
     } else if (length == 16 && (number / 100000000000000 >= 51 && number / 100000000000000 <= 55)) {
         printf("MASTERCARD\n");
-    } else if (length == 15 && (number / 10000000000000 == 4)) {
+    } else if ((length >= 13 && length <= 16) && (number / 10000000000000 == 4)) {
         printf("VISA\n");
     } else {
         printf("INVALID\n");
