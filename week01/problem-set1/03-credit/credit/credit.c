b502d5d848af37eac1188c713d61bc2e11ac1b8b
Laura Andrade <devandrrade@gmail.com>
2024-09-10 12:44:53 +0000

    Tue, Sep 10, 2024, 9:44 AM -03:00

--- week01/problem-set1/03-credit/credit/credit.c
+++ week01/problem-set1/03-credit/credit/credit.c
@@ -6,12 +6,29 @@ int main(void){
         long number = get_long("Number: ");
     } while (number <= 0);
 
-    if(number < 9999999999999){
+    if(number.lenght() < 13){
         printf("INVALID");
     }
     else{
-        if(number >)
+        if(number.lenght() >= 13 && <= 16){
+            if(number starts == 34 || 37){
+
+            }
+        }
+        else{
+            if(number.lenght() == 16){
+                if(number starts >= 51 && <= 55){
+
+            }
+            }
+            else{
+                if(number.lenght() == 15){
+                    if(number starts == 4);
+                }
+                else{
+                    printf("INVALID")
+                }
+            }
+        }
     }
-
-
 }
