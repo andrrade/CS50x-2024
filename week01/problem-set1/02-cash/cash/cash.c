867aab36181aa2f58a58ed5044c14f7acf78f6b8
Laura Andrade <devandrrade@gmail.com>
2024-09-10 11:09:10 +0000

    Tue, Sep 10, 2024, 8:09 AM -03:00

--- week01/problem-set1/02-cash/cash/cash.c
+++ week01/problem-set1/02-cash/cash/cash.c
@@ -1,68 +1,30 @@
 #include <stdio.h>
 #include <cs50.h>
-
-int calculate_quarters(int cents);
-int calculate_dimes(int cents);
-int calculate_nickles(int cents);
-int calculate_pennies(int cents);
+#include <math.h>
 
 int main(void){
-    int cents;
+    float change;
 
     do{
-        cents = get_int("Change owed: ");
+        change = get_int("Change owed: ");
     }
-    while(cents < 0);
+    while(change < 0);
 
-    int quarters = calculate_quarters(cents);
-    cents -= (quarters * 25);
+    int cents = round(change * 100);
 
-    int dimes = calculate_dimes(cents);
-    cents -= (dimes * 10);
+    int quarters = cents / 25;
+    cents =% 25;
 
-    int nickles = calculate_nickles(cents);
-    cents -= (nickles * 5);
+    int dimes = cents / 10;
+    cents =% 10;
 
-    int pennies = calculate_pennies(cents);
-    cents -= (pennies * 1);
+    int nickles = nickles / 5;
+    cents =% 5;
+
+    int pennies = cents / 1;
+    cents =% 1;
 
     int total_coins = quarters + dimes + nickles + pennies;
 
     printf("Total coins: %d\n", total_coins);
-}
-
-int calculate_quarters(int cents){
-    int quarters = 0;
-    while(cents >= 25){
-        quarters++;
-        cents -= 25;
-    }
-    return quarters;
-}
-
-int calculate_dimes (int cents){
-    int dimes = 0;
-    while(dimes >= 10){
-        dimes++;
-        cents -= 10;
-    }
-    return dimes;
-}
-
-int calculate_nickles(int cents){
-    int nickles = 0;
-    while(nickles >= 5){
-        nickles++;
-        cents -= 5;
-    }
-    return nickles;
-}
-
-int calculate_pennies(int cents){
-    int pennies = 0;
-    while(pennies >= 1){
-        pennies++;
-        cents -= 1;
-    }
-    return pennies;
 }
