7ba799792026100393582fb22a56255f9430eae5
Laura Andrade <devandrrade@gmail.com>
2024-09-10 10:44:02 +0000

    Tue, Sep 10, 2024, 7:44 AM -03:00

--- week01/problem-set1/02-cash/cash/cash.c
+++ week01/problem-set1/02-cash/cash/cash.c
@@ -7,13 +7,34 @@ int main(void){
     float m10 = 0.10;
     float m5 = 0.5;
     float m1 = 0.1;
+    int qtde = 0;
 
     do{
         change = get_float("Change owed: ");
     }
     while(change <= 0);
 
-    if(change % m25 == 0){
-        
-    }
+    do{
+        if(change % m25 == 0){
+           change = change/m25;
+           qtde ++;
+        }
+        else{
+            if(change % m10 == 0){
+                change = change/m10;
+                qtde++
+            }
+            else{
+                if(change % m5 == 0){
+                    change = change/m5;
+                    qtde++
+                }
+                else{
+                    if(change % m1 == 0){
+                        change
+                    }
+                }
+            }
+        }
+    } while(change != 0);
 }
