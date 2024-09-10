48a35467847b645679eb092fef627a32d02a5a5e
Laura Andrade <devandrrade@gmail.com>
2024-09-10 11:31:32 +0000

    Tue, Sep 10, 2024, 8:31 AM -03:00

--- week01/problem-set1/02-cash/cash/cash.c
+++ week01/problem-set1/02-cash/cash/cash.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int calculate_coins(int cents, int type);
 
@@ -7,14 +7,12 @@ int main(void)
 {
     int cents;
 
-    // Solicita ao usuário o valor em centavos
     do
     {
         cents = get_int("Change owed: ");
     }
     while (cents < 0);
 
-    // Calcula o número de cada tipo de moeda
     int quarters = calculate_coins(cents, 25);
     cents %= 25;
 
@@ -27,16 +25,14 @@ int main(void)
     int pennies = calculate_coins(cents, 1);
     cents %= 1;
 
-    // Calcula o total de moedas
     int total_coins = quarters + dimes + nickles + pennies;
 
-    // Exibe o total de moedas
-    printf("Total coins: %d\n", total_coins);
+    printf("%d\n", total_coins);
 
     return 0;
 }
 
 int calculate_coins(int cents, int type)
 {
-    return cents / type; // Calcula a quantidade de moedas
+    return cents / type;
 }
