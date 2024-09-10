06b92cdbf10b1880985e296872bbda373fa3d627
Laura Andrade <devandrrade@gmail.com>
2024-09-10 13:12:56 +0000

    Tue, Sep 10, 2024, 10:12 AM -03:00

--- week01/problem-set1/03-credit/credit/credit.c
+++ week01/problem-set1/03-credit/credit/credit.c
@@ -1,12 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 
+long input(long number);
+// long checkSum(long number);
+// long checkCard(long number);
+
 int main(void) {
+
+}
+
+long input(){
     long number;
 
     do {
         number = get_long("Number: ");
     } while (number <= 0);
-
 }
+
+// long checkSum(long number){
+
+// }
+
+// long checkCard(long number){
+
+// }
 
