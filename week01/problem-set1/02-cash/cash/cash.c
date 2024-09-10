2b9a1616aba5ca9542e0cc56414c5c06319ac827
Laura Andrade <devandrrade@gmail.com>
2024-09-10 10:36:53 +0000

    Tue, Sep 10, 2024, 7:36 AM -03:00

--- week01/problem-set1/02-cash/cash/cash.c
+++ week01/problem-set1/02-cash/cash/cash.c
@@ -2,5 +2,14 @@
 #include <cs50.h>
 
 int main(void){
-    
+    int change;
+    int m25 = 0.25;
+    int m10 = 0.10;
+    int m5 = 0.5;
+    int m1 = 0.1;
+
+    do{
+        change = get_int(Change owed: );
+    }
+    while(change <= 0);
 }
