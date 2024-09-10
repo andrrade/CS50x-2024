c9aa477bd8e4afa30ead7c618bb683e3786ae2e1
Laura Andrade <devandrrade@gmail.com>
2024-09-10 13:06:23 +0000

    Tue, Sep 10, 2024, 10:06 AM -03:00

--- week01/problem-set1/03-credit/credit/credit.c
+++ week01/problem-set1/03-credit/credit/credit.c
@@ -8,28 +8,5 @@ int main(void) {
         number = get_long("Number: ");
     } while (number <= 0);
 
-    // Calcula o comprimento do número
-    int length = 0;
-    long temp = number;
-    while (temp > 0) {
-        temp /= 10;
-        length++;
-    }
-
-    // Obtemos o prefixo dos primeiros dígitos para comparação
-    long prefix = number;
-    while (prefix >= 100) {
-        prefix /= 10;
-    }
-
-    if (length == 15 && (prefix == 34 || prefix == 37)) {
-        printf("AMEX\n");
-    } else if (length == 16 && (prefix >= 51 && prefix <= 55)) {
-        printf("MASTERCARD\n");
-    } else if ((length == 13 || length == 16) && (prefix / 10 == 4)) {
-        printf("VISA\n");
-    } else {
-        printf("INVALID\n");
-    }
 }
 
