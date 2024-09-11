a41cd211e0e61e82a9c8d0c097011a4f65b3220a
Laura Andrade <devandrrade@gmail.com>
2024-09-11 13:42:44 +0000

    Wed, Sep 11, 2024, 10:42 AM -03:00

--- week01/pset1/02-mario/mario-less/mario.c
+++ week01/pset1/02-mario/mario-less/mario.c
@@ -1,32 +1,45 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void print_row(int bricks);
+// declarando outras funções além da main;
+void print_row(int spaces, int bricks);
 void print_col(int height);
 
+// função main (principal);
 int main(void){
-    int height;
+    int height; // declarando variável fora do loop;
 
     do{
-        height = get_int("Height: ");
-    } while(height < 1 || height > 8);
+        height = get_int("Height: "); // input para o usuário;
+    } while(height < 1 || height > 8); // definindo valores inválidos;
 
-    print_col(height);
+    print_col(height); // chamando a função que imprime a pirâmide do mário;
 
 }
 
-void print_row(int spaces, int bricks)
+// função que cria as linhas com os espaços e blocos;
+void print_row(int spaces, int bricks) // passando os parâmetros 'qtde de espaços' e  'qtde de blocos' por linha;
 {
+    for (int i = 0; i < spaces; i++){
+        printf(" "); // printando o espaço;
+    }
     for(int i = 0; i < bricks; i++)
     {
-        printf("#");
+        printf("#"); // printando o bloco;
     }
     printf("\n");
 }
 
-void print_col(int height){
+// função que cria as colunas;
+void print_col(int height){ // tem como parâmetro a altura;
     for(int i = 0; i < height; i++){
-        int space = height - i - 1;
-        print_row(i + 1);
+        int spaces = height - i - 1;    // fórmula para fazer a pirâmide invertida, ex: height = 4;
+                                        // espaços serão representados por '.';
+                                        // spaces = height - i - 1;
+                                        // ...# -> i = 0 -> spaces = 4 - 0 - 1 = 3;
+                                        // ..## -> i = 1 -> spaces = 4 - 1 - 1 = 2;
+                                        // .### -> i = 2 -> spaces = 4 - 2 - 1 = 1;
+                                        // #### -> i = 3 -> spaces = 4 - 3 - 1 = 0;
+        print_row(spaces, i + 1);
     }
 }
