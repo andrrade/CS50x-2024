6e9fe8735910684ec6a59c0baf0ac9e562c3140a
Laura Andrade <devandrrade@gmail.com>
2024-09-11 12:47:46 +0000

    Wed, Sep 11, 2024, 9:47 AM -03:00

--- week01/pset1/02-mario/mario-less/mario.c
+++ week01/pset1/02-mario/mario-less/mario.c
@@ -1,34 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void print_row(int spaces, int bricks);
+void print_row(int bricks);
 
-int main(void)
-{
+int main(void){
     int height;
-    do
-    {
-        height = get_int("Height: ");
-    } while (height < 1 || height > 8);
 
-    for (int i = 0; i < height; i++)
-    {
-        int spaces = height - i - 1; // Número de espaços diminui a cada linha
-        int bricks = i + 1;          // Número de blocos aumenta a cada linha
-        print_row(spaces, bricks);
-    }
+    do{
+        height = get_int("Height: ");
+    } while(height < 1 || height > 8);
+
+    print_row(height);
 }
 
-void print_row(int spaces, int bricks)
+void print_row(int bricks);
 {
-    // Imprime os espaços à esquerda
-    for (int i = 0; i < spaces; i++)
-    {
-        printf(" ");
-    }
-
-    // Imprime os blocos
-    for (int i = 0; i < bricks; i++)
+    for(int i = 0; i < bricks; i++)
     {
         printf("#");
     }
