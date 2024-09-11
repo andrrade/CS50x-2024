f79460c81c4593d2a0d90fef433c08febaea9088
Laura Andrade <devandrrade@gmail.com>
2024-09-11 12:18:03 +0000

    Wed, Sep 11, 2024, 9:18 AM -03:00

--- week01/pset1/02-mario/mario-less/mario.c
+++ week01/pset1/02-mario/mario-less/mario.c
@@ -1,20 +1,10 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-int main(void){
-    int n;
-    do {
-        n = get_int("Height: ");
-    }
-    while (n < 1 || n > 8);
+int main(void)
+{
+    do{
+        int height = get_int("Heigh: ");
+    } while(height < 1 || height > 8);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            printf(" ");
-        }
-        for (int j = 0; j <= i; j++) {
-            printf("#");
-        }
-        printf("\n");
-    }
 }
