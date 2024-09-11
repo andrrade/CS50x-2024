66e4c072959757c94c653463c518c9b1646cea8a
Laura Andrade <devandrrade@gmail.com>
2024-09-11 13:59:56 +0000

    Wed, Sep 11, 2024, 10:59 AM -03:00

--- week01/pset1/02-mario/mario-more/mario.c
+++ week01/pset1/02-mario/mario-more/mario.c
@@ -8,30 +8,42 @@ int main(void)
 {
     int height;
 
-    do{
+    do
+    {
         height = get_int("Height: ");
     }
-    while(height < 1 || height > 8);
+    while (height < 1 || height > 8);
 
     print_col(height);
 }
 
-void print_row(int spaces, int bricks){
-    for(int i = 0; i < bricks; i++){
+void print_row(int spaces, int bricks)
+{
+    for (int i = 0; i < spaces; i++)
+    {
+        printf(" ");
+    }
+
+    for (int i = 0; i < bricks; i++)
+    {
         printf("#");
     }
 
-    for(int i = 0; i < spaces; i++){
-        printf(" ");
+    printf("  ");
+
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
     }
     printf("\n");
 }
 
-void print_col(int height){
+void print_col(int height)
+{
 
-    for(int i = 0; i < height; i++){
+    for (int i = 0; i < height; i++)
+    {
         int spaces = height - i - 1;
         print_row(spaces, i + 1);
     }
-
 }
