28deed63e749e4aa8e5e8e44c76bdd428bb6bbdb
Laura Andrade <devandrrade@gmail.com>
2024-09-11 12:20:20 +0000

    Wed, Sep 11, 2024, 9:20 AM -03:00

--- week01/pset1/02-mario/mario-less/mario.c
+++ week01/pset1/02-mario/mario-less/mario.c
@@ -1,10 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_row(int length);
+
 int main(void)
 {
+    int height;
     do{
-        int height = get_int("Heigh: ");
+        height = get_int("Heigh: ");
     } while(height < 1 || height > 8);
 
+    print_row(height);
 }
+
+void print_row(int length);
