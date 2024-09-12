333cfb05260a9fe123cba7851dd42a2ab7f797ea
Laura Andrade <devandrrade@gmail.com>
2024-09-12 09:06:40 +0000

    Thu, Sep 12, 2024, 6:06 AM -03:00

--- week01/pset1/04-credit/credit/credit.c
+++ week01/pset1/04-credit/credit/credit.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-long length(long card);
+long get_length(long card);
 long first_numbers(long all_numbers);
 void what_card(long card_number);
 
@@ -13,12 +13,12 @@ int main(void){
     }
     while (number < 0);
 
-    length(number);
+    get_length(number);
     first_numbers(number);
     what_card(number);
 }
 
-long length(long card)
+long get_length(long card)
 {
     int i = 0;
     while (card > 0)
@@ -41,8 +41,21 @@ long first_numbers(long all_numbers)
 
 void what_card(long card_number)
 {
-    long 
-    if(card_number == 1){
-
+    int length = get_length(card_number);
+    int firsts = first_numbers(card_number);
+    if ((length == 15) && (first_numbers >= 34 || first_numbers <= 37))
+    {
+        printf("AMEX\n");
+    }
+    else if ((length == 16) && (first_numbers >= 51 && first_numbers <= 55))
+    {
+        printf("MASTERCARD\n");
+    }
+    else if ((length >= 13 && length <= 16) && ((first_numbers / 10) == 4))
+    {
+        printf("VISA\n");
+    }
+    else{
+        printf("INVALID\n");
     }
 }
