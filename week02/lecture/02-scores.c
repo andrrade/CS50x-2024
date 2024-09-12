0884f41312a83db286bf5b71d0908e45981ac340
Laura Andrade <devandrrade@gmail.com>
2024-09-12 17:29:15 +0000

    Thu, Sep 12, 2024, 2:29 PM -03:00

--- week02/lecture/02-scores.c
+++ week02/lecture/02-scores.c
@@ -1,14 +1,23 @@
+// Averages three numbers using array, a constant, and a helper function
+
 #include <cs50.h>
 #include <stdio.h>
+
+// Constant
 const int N;
+
+// Prototype
+float average(int length, int array[]);
 
 int main(void)
 {
+    // Get scores
     int scores[N]; // declara que o vetor terá 3 espaços na memória
-
     for (int i = 0; i < N; i++)
     {
         scores[i] = get_int("Score: ");
     }
+
+    // print Average
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) N);
 }
