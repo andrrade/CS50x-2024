dc6f36f951da2e5732d417f6e8342d5f330c256e
Laura Andrade <devandrrade@gmail.com>
2024-09-12 17:17:11 +0000

    Thu, Sep 12, 2024, 2:17 PM -03:00

--- week02/lecture/02-scores.c
+++ week02/lecture/02-scores.c
@@ -5,9 +5,9 @@ int main(void)
 {
     int scores[3]; // declara que o vetor terá 3 espaços na memória
 
-    for()
+    for (int i = 0; i < 3; i++)
     {
-        
+        scores[i] = get_int("Score: ");
     }
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
