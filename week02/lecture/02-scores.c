4660d23947dbfa6ccb1cc40b14b526fdc986bcd7
Laura Andrade <devandrrade@gmail.com>
2024-09-12 17:11:57 +0000

    Thu, Sep 12, 2024, 2:11 PM -03:00

--- week02/lecture/02-scores.c
+++ week02/lecture/02-scores.c
@@ -5,9 +5,9 @@ int main(void)
 {
     int scores[3]; // declara que o vetor terá 3 espaços na memória
 
-    scores[0] = get_int("Score: "); // armazena o valor na primeira posição do vetor
-    scores[1] = get_int("Score: "); // armazena o valor na segunda posição do vetor
-    scores[2] = get_int("Score: "); // armazena o valor na terceira posição do vetor
-
+    for()
+    {
+        
+    }
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
