4d7316072dc7988cf30b24ef947f73c99b6d0081
Laura Andrade <devandrrade@gmail.com>
2024-09-13 12:17:02 +0000

    Fri, Sep 13, 2024, 9:17 AM -03:00

--- week02/lecture/04-length.c
+++ week02/lecture/04-length.c
@@ -1,15 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int length_string(string s);
+
 int main(void)
 {
   string name = get_string("Name: ");
+  length_string(name);
+}
 
+int length_string(string s)
+{
   int n = 0;
 
-  while(name[n] != '\0')
+  while(s[n] != '\0')
   {
     n++;
   }
-  printf("%i\n", n);
+  return n;
 }
