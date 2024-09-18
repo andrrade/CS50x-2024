d8d14f22c3895e9d80387df48ff468f02010582d
Laura Andrade <devandrrade@gmail.com>
2024-09-18 22:45:04 +0000

    Wed, Sep 18, 2024, 7:45 PM -03:00

--- week03/lecture/04-iteration.c
+++ week03/lecture/04-iteration.c
@@ -11,12 +11,12 @@ int main(void)
 
 void draw(int n)
 {
-    for (int i = 0; i < height; i++)
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < i + 1; j++)
         {
             printf("#");
         }
-        printf("\n")
+        printf("\n");
     }
 }
