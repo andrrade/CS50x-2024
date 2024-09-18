c7bb3d7a4c8c2f652c3d3bef8383ca77c98f1839
Laura Andrade <devandrrade@gmail.com>
2024-09-18 17:18:07 +0000

    Wed, Sep 18, 2024, 2:18 PM -03:00

---
+++ week03/lecture/phonebook.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string names[] = {"Carter", "David", "John"};
+    string numbers[] = {"+1-617-495-1000", "+1-617-495-1000", "+1-949-468-2750"};
+}
