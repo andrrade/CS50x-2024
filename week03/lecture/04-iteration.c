b97c108145a488331f7c02e281cb69dc5c6379fc
Laura Andrade <devandrrade@gmail.com>
2024-09-18 22:43:53 +0000

    Wed, Sep 18, 2024, 7:43 PM -03:00

--- week03/lecture/04-iteration.c
+++ week03/lecture/04-iteration.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void draw(int n);
+
 int main(void)
 {
     int height = get_int("Height: ");
@@ -9,5 +11,12 @@ int main(void)
 
 void draw(int n)
 {
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n")
+    }
 }
