93015e47ca3253a345c45fa7ae843c38d69615a3
Laura Andrade <devandrrade@gmail.com>
2024-09-23 17:29:32 +0000

    Mon, Sep 23, 2024, 2:29 PM -03:00

--- week02/pset2/02-readbility/readbility.c
+++ week02/pset2/02-readbility/readbility.c
@@ -4,30 +4,36 @@
 #include <stdio.h>
 #include <string.h>
 
-int count_letters(string text);
-int count_words(string text);
-int count_sentences(string text);
+
 
 int main(void)
 {
     string text = get_string("Text: ");
+    int length = strlen(text);
     int letters = count_letters(text);
     int words = count_words(text);
     int sentences = count_sentences(text);
 
-}
 
-int count_letters(string text)
-{
-    
-}
 
-int count_words(string text)
-{
 
 }
 
-int count_sentences(string text)
+int count(int type, string text, int length)
 {
-
+    for (int i = 0; i < length; i++)
+    {
+        if(isalpha(text[i]))
+        {
+            letters++;
+        }
+        if(isspace(text[i]))
+        {
+            words++;
+        }
+        if(text[i] == "." || text[i] == "!" || text[i] == "?")
+        {
+            sentences++;
+        }
+    }
 }
