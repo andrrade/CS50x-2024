d9e0dc0593c597d2926d0f8eafa26f2bfbe1319e
Laura Andrade <devandrrade@gmail.com>
2024-09-23 17:32:49 +0000

    Mon, Sep 23, 2024, 2:32 PM -03:00

--- week02/pset2/02-readbility/readbility.c
+++ week02/pset2/02-readbility/readbility.c
@@ -4,36 +4,47 @@
 #include <stdio.h>
 #include <string.h>
 
-
+int count(int type, string text, int length);
 
 int main(void)
 {
     string text = get_string("Text: ");
-    int length = strlen(text);
-    int letters = count_letters(text);
-    int words = count_words(text);
-    int sentences = count_sentences(text);
-
-
-
-
+    int length = strlen(text, length);
+    int letters = count_letters(text, length);
+    int words = count_words(text, length);
+    int sentences = count_sentences(text, length);
 }
 
-int count(int type, string text, int length)
+int count_letters(string text, int length)
 {
+    int letters = 0;
     for (int i = 0; i < length; i++)
     {
         if(isalpha(text[i]))
         {
             letters++;
         }
-        if(isspace(text[i]))
+
+    }
+    return letters;
+}
+
+int count_words(string text, int length)
+{
+    int words = 0;
+     if(isspace(text[i]))
         {
             words++;
         }
-        if(text[i] == "." || text[i] == "!" || text[i] == "?")
+    return words++;
+}
+
+int count_sentences(string text, int length)
+{
+    int sentences = 0;
+     if(text[i] == "." || text[i] == "!" || text[i] == "?")
         {
             sentences++;
         }
-    }
+        return sentences;
 }
