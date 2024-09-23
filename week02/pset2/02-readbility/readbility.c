9f950ea43f3720322ffcc880394f63b7ca5bf4b4
Laura Andrade <devandrrade@gmail.com>
2024-09-23 17:16:08 +0000

    Mon, Sep 23, 2024, 2:16 PM -03:00

---
+++ week02/pset2/02-readbility/readbility.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
+    
+}
