7829907434ad101df407e1d539adafe2fd2c0b7b
Laura Andrade <devandrrade@gmail.com>
2024-10-03 17:13:26 +0000

    Thu, Oct 3, 2024, 2:13 PM -03:00

---
+++ week03/section/01-search.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+typedef struct {
+    string name;
+    int votes;
+}
+candidate;
+
+int main(void)
+{
+    const int num_candidates = 3;
+    candidate candidates[num_candidates];
+
+    candidates[0].name = "Carter";
+    candidates[0].votes = 10;
+
+    candidates[1].name = "Yuliia";
+    candidates[1].votes = 12;
+
+    candidates[2].name = "Inno";
+    candidates[2].votes = 7;
+}
