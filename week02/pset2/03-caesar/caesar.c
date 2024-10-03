f41a308eff3c54b7f8a8dc30e24ca04bdf3cd83b
Laura Andrade <devandrrade@gmail.com>
2024-10-03 01:24:07 +0000

    Wed, Oct 2, 2024, 10:24 PM -03:00

--- week02/pset2/03-caesar/caesar.c
+++ week02/pset2/03-caesar/caesar.c
@@ -4,15 +4,12 @@
 #include <ctype.h>
 #include <string.h>
 
-int main(int argc, int argv[]) // Mantendo conforme solicitado
+int main(int argc, char *argv[])
 {
     if (argc != 2) {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-
-    // Convertendo o valor de argv[1] para string
-    string key = (char *) argv[1]; // É necessário forçar a conversão para string.
 
     // Verificando se todos os caracteres da key são dígitos
     for (int i = 0; i < strlen(key); i++) {
