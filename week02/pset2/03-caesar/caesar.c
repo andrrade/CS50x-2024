aaec73fc00ed4da1b6f3a2820a7020df20b8f147
Laura Andrade <devandrrade@gmail.com>
2024-10-03 01:29:16 +0000

    Wed, Oct 2, 2024, 10:29 PM -03:00

--- week02/pset2/03-caesar/caesar.c
+++ week02/pset2/03-caesar/caesar.c
@@ -1,21 +1,24 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
 int main(int argc, string argv[])
 {
     // Verifica se o número correto de argumentos foi passado
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
     // Verifica se a key contém apenas dígitos
     string key = argv[1];
-    for (int i = 0; i < strlen(key); i++) {
-        if (!isdigit(key[i])) {
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (!isdigit(key[i]))
+        {
             printf("Usage: ./caesar key\n");
             return 1;
         }
@@ -30,18 +33,23 @@ int main(int argc, string argv[])
     printf("Ciphertext: ");
 
     // Criptografa o texto
-    for (int i = 0; i < strlen(text); i++) {
-        if (isalpha(text[i])) {
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
             // Para letras maiúsculas
-            if (isupper(text[i])) {
+            if (isupper(text[i]))
+            {
                 printf("%c", (text[i] - 'A' + key_num) % 26 + 'A');
             }
             // Para letras minúsculas
-            else if (islower(text[i])) {
+            else if (islower(text[i]))
+            {
                 printf("%c", (text[i] - 'a' + key_num) % 26 + 'a');
             }
         }
-        else {
+        else
+        {
             // Se não for letra, imprime o caractere sem alteração
             printf("%c", text[i]);
         }
@@ -50,4 +58,3 @@ int main(int argc, string argv[])
     printf("\n");
     return 0;
 }
-
