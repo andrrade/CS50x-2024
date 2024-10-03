730a2e274f1b0e4df2202463e6528ee0b6c50e4a
Laura Andrade <devandrrade@gmail.com>
2024-10-03 01:22:00 +0000

    Wed, Oct 2, 2024, 10:22 PM -03:00

--- week02/pset2/03-caesar/caesar.c
+++ week02/pset2/03-caesar/caesar.c
@@ -4,39 +4,48 @@
 #include <ctype.h>
 #include <string.h>
 
-int main(int argc, int argv[])
+int main(int argc, int argv[]) // Mantendo conforme solicitado
 {
-    if (argc != 2){
-        printf("Usage: ./caesar key\n")
+    if (argc != 2) {
+        printf("Usage: ./caesar key\n");
+        return 1;
     }
 
-    string key = argv[1];
-
-    for (int i = 0; i < strlen(key); i++)
-    {
-        if(!isdigit(key[i]))
-        {
+    
+    // Verificando se todos os caracteres da key são dígitos
+    for (int i = 0; i < strlen(key); i++) {
+        if (!isdigit(key[i])) {
             printf("Usage: ./caesar key\n");
             return 1;
         }
     }
 
+    // Convertendo a key para número
     int key_num = atoi(key);
 
+    // Solicitando o texto do usuário
     string text = get_string("Plaintext: ");
 
-    for (int i = 0; i < strlen(text); i++)
-    {
-        if(isalpha(text[i]))
-        {
-            string cypher = (text[i] + key_num) % 26;
-            if(isupper(text[i])) {
-                cypher += cypher;
+    printf("Ciphertext: ");
+
+    // Criptografando o texto
+    for (int i = 0; i < strlen(text); i++) {
+        if (isalpha(text[i])) {
+            // Para letras maiúsculas
+            if (isupper(text[i])) {
+                printf("%c", (text[i] - 'A' + key_num) % 26 + 'A');
             }
-            else {
-                cypher += cypher;
+            // Para letras minúsculas
+            else if (islower(text[i])) {
+                printf("%c", (text[i] - 'a' + key_num) % 26 + 'a');
             }
+        }
+        else {
+            // Se não for letra, imprime o caractere sem alteração
+            printf("%c", text[i]);
         }
     }
 
+    printf("\n");
+    return 0;
 }
