0c1b02c51dba7338d7a63cc27c43f324b96cdb22
Laura Andrade <devandrrade@gmail.com>
2024-10-04 16:51:44 +0000

    Fri, Oct 4, 2024, 1:51 PM -03:00

--- week04/lecture/04-compare.c
+++ week04/lecture/04-compare.c
@@ -9,7 +9,7 @@ int main(void)
     char *t = get_string("t: ");
     // p.s. s and t are adresses of the first character, no strings
 
-    if (strcmp(s, t))
+    if (strcmp(s, t) == 0) // 0 = same; positive number = one comes before another; negative number = if the opposite is true.
     {
         printf("Same\n");
     }
