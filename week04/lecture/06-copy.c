2038b64c53e1c56c6773244a0af4c16c5784e20e
Laura Andrade <devandrrade@gmail.com>
2024-10-04 17:30:06 +0000

    Fri, Oct 4, 2024, 2:30 PM -03:00

--- week04/lecture/06-copy.c
+++ week04/lecture/06-copy.c
@@ -10,6 +10,11 @@ int main(void)
 
     char *t = malloc(strlen(s) + 1);
     // malloc = memory allocate
+    if (t == NULL)
+    {
+        return 1;
+    }
+
 
     for (int i = 0, n = strlen(s); i <= n; i++)
     {
