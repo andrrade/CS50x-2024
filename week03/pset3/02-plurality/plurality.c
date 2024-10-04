e65738d189c5016a0bbbd2d0b939ad4c966f5973
Laura Andrade <devandrrade@gmail.com>
2024-10-04 03:26:33 +0000

    Fri, Oct 4, 2024, 12:26 AM -03:00

--- week03/pset3/02-plurality/plurality.c
+++ week03/pset3/02-plurality/plurality.c
@@ -79,13 +79,13 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int heighest = 0;
+    int highest = 0;
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > heighest)
+        if (candidates[i].votes > highest)
         {
-            heighest = candidates[i].votes;
+            highest = candidates[i].votes;
         }
     }
 
