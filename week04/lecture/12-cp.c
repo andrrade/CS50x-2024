fc6931e452abb7e349200a41890eb599aca175e3
Laura Andrade <devandrrade@gmail.com>
2024-10-05 13:38:22 +0000

    Sat, Oct 5, 2024, 10:38 AM -03:00

--- week04/lecture/12-cp.c
+++ week04/lecture/12-cp.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 
     BYTE b;
 
-    while (fread(&b, sizeof(b), 1, src) != 0)
+    while (fread(&b, sizeof(b), 1, src) != 0) // b = pass by value; &b = pass by reference
     {
         fwrite(&b, sizeof(b), 1, dst);
     }
