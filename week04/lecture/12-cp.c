afc2a610d612cb6964ca97b7d4a41959af05bb88
Laura Andrade <devandrrade@gmail.com>
2024-10-05 13:37:37 +0000

    Sat, Oct 5, 2024, 10:37 AM -03:00

--- week04/lecture/12-cp.c
+++ week04/lecture/12-cp.c
@@ -5,5 +5,15 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    
+    FILE *src = fopen(argv[1], "rb"); // rb = read binary mode;
+    FILE *dst = fopen(argv[2], "wb"); // wb = write to it in binary
+
+    BYTE b;
+
+    while (fread(&b, sizeof(b), 1, src) != 0)
+    {
+        fwrite(&b, sizeof(b), 1, dst);
+    }
+    fclose(dst);
+    fclose(src);
 }
