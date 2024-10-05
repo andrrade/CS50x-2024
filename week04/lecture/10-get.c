fc255eea963165843aa5b43234ff6a4e5d5f98c0
Laura Andrade <devandrrade@gmail.com>
2024-10-05 13:09:35 +0000

    Sat, Oct 5, 2024, 10:09 AM -03:00

--- week04/lecture/10-get.c
+++ week04/lecture/10-get.c
@@ -2,8 +2,15 @@
 
 int main(void)
 {
-    int n;
-    printf("n: ");
-    scanf("%i", &n);
-    printf("n: %i\n", n);
+    // get_int:
+    // int n;
+    // printf("n: ");
+    // scanf("%i", &n);
+    // printf("n: %i\n", n);
+
+    // get_string
+    char *s; // char * is already an adress and don't need ampersand (&), like integers.
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s)
 }
