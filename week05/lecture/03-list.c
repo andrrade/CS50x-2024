595b01fd118664c75efce05d14391810297fdede
Laura Andrade <devandrrade@gmail.com>
2024-10-07 17:22:29 +0000

    Mon, Oct 7, 2024, 2:22 PM -03:00

--- week05/lecture/03-list.c
+++ week05/lecture/03-list.c
@@ -54,4 +54,13 @@ int main(int argc, char *argv[])
             }
         }
     }
+
+    // Print whole list
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
+    }
+    // O(n) - for insertion
 }
