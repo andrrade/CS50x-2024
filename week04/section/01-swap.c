ec564484f5021870b4547139994a83bf42ed211e
Laura Andrade <devandrrade@gmail.com>
2024-10-07 11:21:01 +0000

    Mon, Oct 7, 2024, 8:21 AM -03:00

--- week04/section/01-swap.c
+++ week04/section/01-swap.c
@@ -6,4 +6,16 @@ void swap(int *a, int *b);
 int main(void)
 {
     int a = 10;
+    int b = 50;
+
+    printf("a is %i, b is %i\n", a, b);
+    swap(&a, &b);
+    printf("a is %i, b is %i\n", a, b);
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
