503617ff5e9a42458ff6d366108fb135871c6d6b
Laura Andrade <devandrrade@gmail.com>
2024-10-07 15:02:17 +0000

    Mon, Oct 7, 2024, 12:02 PM -03:00

--- week05/lecture/01-list.c
+++ week05/lecture/01-list.c
@@ -16,8 +16,18 @@ int main(void)
     int *tmp = malloc(4 * sizeof(int));
     if (temp == NULL)
     {
+        free(list); // good practice
         return 1;
     }
+
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i]; // copying into the temporary array whatever was in the old array
+    }
+    tmp[3] = 4;
+    free(list);
+    list = tmp;
+
     for (int i = 0; i < 3; i++)
     {
         printf("%i\n", list[i]);
