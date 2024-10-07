b9cbf603624db3204fa64576caaed6d0b05eead4
Laura Andrade <devandrrade@gmail.com>
2024-10-07 23:02:02 +0000

    Mon, Oct 7, 2024, 8:02 PM -03:00

--- week03/pset3/03-runoff/runoff.c
+++ week03/pset3/03-runoff/runoff.c
@@ -128,10 +128,12 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    for (int i = 0; i < ; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        
-
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            return true;
+        }
     }
     return false;
 }
