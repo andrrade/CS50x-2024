96a1a4fe9a0efa899d70f08ab966ce4643c337df
Laura Andrade <devandrrade@gmail.com>
2024-10-07 14:57:24 +0000

    Mon, Oct 7, 2024, 11:57 AM -03:00

--- week05/lecture/01-list.c
+++ week05/lecture/01-list.c
@@ -8,11 +8,16 @@ int main(void)
     {
         return 1;
     }
-    
+
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
 
+    int *tmp = malloc(4 * sizeof(int));
+    if (temp == NULL)
+    {
+        return 1;
+    }
     for (int i = 0; i < 3; i++)
     {
         printf("%i\n", list[i]);
