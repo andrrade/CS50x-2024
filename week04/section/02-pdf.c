f5137c29704c66694c396dd3d96d903111e2773d
Laura Andrade <devandrrade@gmail.com>
2024-10-07 13:54:29 +0000

    Mon, Oct 7, 2024, 10:54 AM -03:00

--- week04/section/02-pdf.c
+++ week04/section/02-pdf.c
@@ -7,5 +7,11 @@ int main(int argc, string argv[])
     string filename = argv[1];
     FILE *f = fopen(filename, "r");
     uint8_t buffer[4];
+    fread(buffer, 1, 4, f);
 
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", buffer[i]);
+    }
+    fclose(f);
 }
