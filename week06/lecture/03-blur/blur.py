526480a9572cdc525312411b3ea0795f2eb37cf4
Laura Andrade <devandrrade@gmail.com>
2024-10-08 17:06:41 +0000

    Tue, Oct 8, 2024, 2:06 PM -03:00

--- week06/lecture/03-blur/blur.py
+++ week06/lecture/03-blur/blur.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageFilter
 
 before = Image.open("yard.bmp")
-after = before.filter(ImageFilter.BoXBlur(10))
+after = before.filter(ImageFilter.BoxBlur(10))
 after.save("out.bmp")
