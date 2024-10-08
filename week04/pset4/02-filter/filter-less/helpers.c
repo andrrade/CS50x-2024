8e0bb050ee4387c8abc9ae38fc15c12e1a5536cd
Laura Andrade <devandrrade@gmail.com>
2024-10-08 09:09:17 +0000

    Tue, Oct 8, 2024, 6:09 AM -03:00

--- week04/pset4/02-filter/filter-less/helpers.c
+++ week04/pset4/02-filter/filter-less/helpers.c
@@ -26,21 +26,119 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
-
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop through each row of pixels
+    for (int i = 0; i < height; i++)
+    {
+        // Loop through each pixel in the row
+        for (int j = 0; j < width; j++)
+        {
+            // Get the original red, green, and blue values of the current pixel
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            // Apply sepia formula
+            int sepiaRed = (int)(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            int sepiaGreen = (int)(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            int sepiaBlue = (int)(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+
+            // Make sure the values don't exceed 255
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
+            // Set the pixel's new red, green, and blue values
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
+
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop through each row of pixels
+    for (int i = 0; i < height; i++)
+    {
+        // Loop through half of the row
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Swap the pixel on the left with the pixel on the right
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = temp;
+        }
+    }
+
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Create a temporary copy of the image to store the original values
+    RGBTRIPLE temp[height][width];
+
+    // Copy the original image into temp
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp[i][j] = image[i][j];
+        }
+    }
+
+    // Loop through each pixel in the image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int redSum = 0;
+            int greenSum = 0;
+            int blueSum = 0;
+            int counter = 0;
+
+            // Loop through the neighboring pixels in a 3x3 box
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
+
+                    // Check if neighboring pixel is within image bounds
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    {
+                        redSum += temp[ni][nj].rgbtRed;
+                        greenSum += temp[ni][nj].rgbtGreen;
+                        blueSum += temp[ni][nj].rgbtBlue;
+                        counter++;
+                    }
+                }
+            }
+
+            // Calculate the average color values
+            image[i][j].rgbtRed = redSum / counter;
+            image[i][j].rgbtGreen = greenSum / counter;
+            image[i][j].rgbtBlue = blueSum / counter;
+        }
+    }
+
     return;
 }
+
