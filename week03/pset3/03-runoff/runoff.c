b81282923ccdd7560cc0a604b81636577dac9d3a
Laura Andrade <devandrrade@gmail.com>
2024-10-08 02:02:54 +0000

    Mon, Oct 7, 2024, 11:02 PM -03:00

--- week03/pset3/03-runoff/runoff.c
+++ week03/pset3/03-runoff/runoff.c
@@ -193,12 +193,12 @@ bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if(candidates[i].votes == min)
+        if(candidates[i].votes != min)
         {
-            
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
