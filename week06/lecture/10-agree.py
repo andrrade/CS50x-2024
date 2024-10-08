ba294378eabc9a78a4de25ec7615e4e58e8154a2
Laura Andrade <devandrrade@gmail.com>
2024-10-08 19:26:18 +0000

    Tue, Oct 8, 2024, 4:26 PM -03:00

--- week06/lecture/10-agree.py
+++ week06/lecture/10-agree.py
@@ -1,7 +1,14 @@
 s = input("Do you agree? [S/N]: ")
-if (s == "Y") or (s == "y"):
+
+# if (s == "Y") or (s == "y"):
+#     print("Yes :)")
+# elif (s == "N") or (s == "n"):
+#     print("No :(")
+# else:
+#     print("Invalid Option :|")
+if s in ["y", "yes"]:
     print("Yes :)")
-elif (s == "N") or (s == "n"):
+elif n in ["n", "no"]:
     print("No :(")
 else:
-    print("Invalid Option :|")
+    print("Invalid :|")
