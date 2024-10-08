4cff1a480bc1028980dc58e9f8c5f81d376641d6
Laura Andrade <devandrrade@gmail.com>
2024-10-08 01:58:08 +0000

    Mon, Oct 7, 2024, 10:58 PM -03:00

--- week03/pset3/03-runoff/runoff.c
+++ week03/pset3/03-runoff/runoff.c
@@ -174,7 +174,7 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    if (i)
+    int minimum
     for (int i = 0; i < candidate_count; i++)
     {
         if (!candidates[i].eliminated)
