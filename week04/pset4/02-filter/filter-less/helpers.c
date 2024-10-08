bf4f08f7f0371c12d40558940c55a80c126bcc04
Laura Andrade <devandrrade@gmail.com>
2024-10-08 09:07:02 +0000

    Tue, Oct 8, 2024, 6:07 AM -03:00

--- week04/pset4/02-filter/filter-less/helpers.c
+++ week04/pset4/02-filter/filter-less/helpers.c
@@ -3,8 +3,29 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop through each row of pixels
+    for (int i = 0; i < height; i++)
+    {
+        // Loop through each pixel in the row
+        for (int j = 0; j < width; j++)
+        {
+            // Get the red, green, and blue values of the current pixel
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+
+            // Calculate the average of the three colors
+            int average = (red + green + blue) / 3;
+
+            // Set each color to the average value
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
     return;
 }
+
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
