e18ad865200d9bb1feed0b95947919fb5cbcbbed
Laura Andrade <devandrrade@gmail.com>
2024-10-08 19:13:46 +0000

    Tue, Oct 8, 2024, 4:13 PM -03:00

--- week06/lecture/08-compare.py
+++ week06/lecture/08-compare.py
@@ -4,5 +4,8 @@ x = get_int("What's x? ")
 y = get_int("What's y? ")
 
 if x < y:
-    print("x < y")
-elif x >
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
