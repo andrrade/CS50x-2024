7ad2d02dc9d4bebe38229ecd72da24bdbb479377
Laura Andrade <devandrrade@gmail.com>
2024-10-08 08:38:49 +0000

    Tue, Oct 8, 2024, 5:38 AM -03:00

--- week04/pset4/01-volume/01-volume/volume.c
+++ week04/pset4/01-volume/01-volume/volume.c
@@ -42,7 +42,9 @@ int main(int argc, char *argv[])
     int16_t buffer;
     while (fread(&buffer, sizeof(int16_t), 1, input))
     {
-        
+        // Update volume
+        buffer *= factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
     }
     // Close files
     fclose(input);
