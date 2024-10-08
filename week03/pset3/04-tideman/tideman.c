091d88ffe3671b926aeec5269eee33bfbb621430
Laura Andrade <devandrrade@gmail.com>
2024-10-08 02:24:27 +0000

    Mon, Oct 7, 2024, 11:24 PM -03:00

--- week03/pset3/04-tideman/tideman.c
+++ week03/pset3/04-tideman/tideman.c
@@ -27,7 +27,7 @@ int candidate_count;
 // Function prototypes
 bool vote(int rank, string name, int ranks[]);
 void record_preferences(int ranks[]);
-void add_pairs(void);   
+void add_pairs(void);
 void sort_pairs(void);
 void lock_pairs(void);
 void print_winner(void);
@@ -98,7 +98,7 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    for (int i = 0; i < )
     return false;
 }
 
