a092281518b9bb6c4d3db8f0c39b434dfbe0396a
Laura Andrade <devandrrade@gmail.com>
2024-10-08 18:10:03 +0000

    Tue, Oct 8, 2024, 3:10 PM -03:00

--- week06/lecture/07-calculator/calculator.py
+++ week06/lecture/07-calculator/calculator.py
@@ -1,7 +1,7 @@
-from cs50 import get_int
+# from cs50 import get_int
 
-x = get_int("x: ")
-y = get_int("y: ")
+x = int(input("x: "))
+y = int(input("y: "))
 
 # print(x + y)
 print(f"{x} + {y} = {x + y}")
