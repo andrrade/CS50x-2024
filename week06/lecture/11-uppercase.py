8c09cf93499e72bf570f4f3fb3638ea856db1f6c
Laura Andrade <devandrrade@gmail.com>
2024-10-08 20:05:27 +0000

    Tue, Oct 8, 2024, 5:05 PM -03:00

--- week06/lecture/11-uppercase.py
+++ week06/lecture/11-uppercase.py
@@ -1,6 +1,6 @@
 before = input("Before: ")
-after = before.upper()
-print(f"After:  {after}")
+# after = before.upper()
+print(f"After:  {before.upper()}")
 
 # for c in before:
 #     print(c.upper(), end="")
