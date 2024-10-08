9ce57d9df3bd05afc222d3d002ddbae490e0fb65
Laura Andrade <devandrrade@gmail.com>
2024-10-08 02:08:04 +0000

    Mon, Oct 7, 2024, 11:08 PM -03:00

--- week03/pset3/03-runoff/runoff.c
+++ week03/pset3/03-runoff/runoff.c
@@ -161,12 +161,11 @@ bool print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > voter_count/2)
+        if (candidates[i].votes > voter_count / 2)
         {
             printf("%s\n", candidates[i].name);
             return true;
         }
-
     }
     return false;
 }
@@ -195,7 +194,7 @@ bool is_tie(int min)
     {
         if (!candidates[i].eliminated)
         {
-            if(candidates[i].votes != min)
+            if (candidates[i].votes != min)
             {
                 return false;
             }
