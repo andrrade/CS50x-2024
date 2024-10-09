60bec6e5f664df685fa3c7b2c4ef6b81ab674fb6
Laura Andrade <devandrrade@gmail.com>
2024-10-09 12:37:58 +0000

    Wed, Oct 9, 2024, 9:37 AM -03:00

--- week06/lecture/14-mario.py
+++ week06/lecture/14-mario.py
@@ -8,7 +8,15 @@
 # for _ in range(n):
 #     print("#")
 
-for i in range(4):
-    print("?", end="")
 
-print()
+# for i in range(4):
+#     print("?", end="")
+
+# print()
+
+# print("?" * 4)
+
+for i in range(3):
+    for j in range(3):
+        print("#", end="")
+    print()
