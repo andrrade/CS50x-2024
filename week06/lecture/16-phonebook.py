2c9b7a6468fd364479ec03ca086937c49c7e0e3a
Laura Andrade <devandrrade@gmail.com>
2024-10-09 13:14:11 +0000

    Wed, Oct 9, 2024, 10:14 AM -03:00

--- week06/lecture/16-phonebook.py
+++ week06/lecture/16-phonebook.py
@@ -2,9 +2,14 @@ names = ["Carter", "David", "John"]
 
 name = input("Name: ")
 
-for n in names:
-    if (name == n):
-        print("Found")
-        break
-else: # for lloops can have an else clause
+# for n in names:
+#     if (name == n):
+#         print("Found")
+#         break
+# else: # for lloops can have an else clause
+#     print("Not found")
+
+if name in names:
+    print("Found")
+else:
     print("Not found")
