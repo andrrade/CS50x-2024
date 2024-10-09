97f46157c28065d91497b29d23488929ae62eeaf
Laura Andrade <devandrrade@gmail.com>
2024-10-09 12:14:30 +0000

    Wed, Oct 9, 2024, 9:14 AM -03:00

--- week06/lecture/13-calculator.py
+++ week06/lecture/13-calculator.py
@@ -1,5 +1,8 @@
-x = int(input("x: "))
-y = int(input("y: "))
+# x = int(input("x: "))
+# y = int(input("y: "))
 
-z = x / y
-print(f"{z:.50f}")
+# z = x / y
+# print(f"{z:.50f}")
+
+def get_int(prompt):
+    return int(input(prompt))
