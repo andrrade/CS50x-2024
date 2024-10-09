9ffa0348ea1586702a4ffd57e71bca9579d0d8a2
Laura Andrade <devandrrade@gmail.com>
2024-10-09 12:33:28 +0000

    Wed, Oct 9, 2024, 9:33 AM -03:00

--- week06/lecture/14-mario.py
+++ week06/lecture/14-mario.py
@@ -1,9 +1,14 @@
-from cs50 import get_int
+# from cs50 import get_int
 
-while True:
-    n = get_int("Height: ")
-    if (n > 0):
-        break # We've got the value we need
+# while True:
+#     n = get_int("Height: ")
+#     if (n > 0):
+#         break # We've got the value we need
 
-for _ in range(n):
-    print("#")
+# for _ in range(n):
+#     print("#")
+
+for i in range(4):
+    print("?", end="")
+
+print()
