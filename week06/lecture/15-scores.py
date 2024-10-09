6f66a213620bab50cbb5ba68ba4cfe4600fd292f
Laura Andrade <devandrrade@gmail.com>
2024-10-09 13:06:08 +0000

    Wed, Oct 9, 2024, 10:06 AM -03:00

--- week06/lecture/15-scores.py
+++ week06/lecture/15-scores.py
@@ -4,6 +4,8 @@ scores = []
 for _ in range(3):
     score = get_int("Score: ")
     scores.append(score)
+    # or
+    # scores += [score]
 
 average = sum(scores) / len(scores)
 print(f"Average: {average}")
