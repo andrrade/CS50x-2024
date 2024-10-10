ea2cd011b46365728881a0181d5951c39ca99cfe
Laura Andrade <devandrrade@gmail.com>
2024-10-10 13:54:41 +0000

    Thu, Oct 10, 2024, 10:54 AM -03:00

---
+++ week06/lecture/19-exit.py
@@ -0,0 +1,12 @@
+# import sys
+
+# if len(sys.argv)
+
+# from sys import argv
+import sys
+
+if len(sys.argv) != 2:
+    print("Missing command-line argument")
+    sys.exit(1)
+print(f"hello, {sys.argv[1]}")
+sys.exit(0)
