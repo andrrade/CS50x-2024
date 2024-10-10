73793885be9143caee511de9a5b1cc35e0408005
Laura Andrade <devandrrade@gmail.com>
2024-10-10 13:44:04 +0000

    Thu, Oct 10, 2024, 10:44 AM -03:00

--- week06/lecture/17-phonebook.py
+++ week06/lecture/17-phonebook.py
@@ -1,12 +1,18 @@
-people = [
-    {
-        "name": "Carter",
-        "number": "+1-617-495-1000"
-    },
+# people = [
+#     {
+#         "name": "Carter",
+#         "number": "+1-617-495-1000"
+#     },
 
-    {"name": "David", "number": "+1-617-495-1000"},
-    {"name": "John", "number": "+1-949-468-2750"},
-]
+#     {"name": "David", "number": "+1-617-495-1000"},
+#     {"name": "John", "number": "+1-949-468-2750"},
+# ]
+
+people = {
+    "Carter": "+1-617-495-1000",
+    "David": "+1-617-495-1000",
+    "John": "+1-949-468-2750",
+}
 
 name = input("Name: ")
 
