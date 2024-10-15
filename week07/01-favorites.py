ea276df6fbf55c6cef7162d5c148b9562f1dcf69
Laura Andrade <devandrrade@gmail.com>
2024-10-15 13:33:15 +0000

    Tue, Oct 15, 2024, 10:33 AM -03:00

--- week07/01-favorites.py
+++ week07/01-favorites.py
@@ -6,20 +6,34 @@ import csv
 
 with open("favorites.csv", "r") as file: # "witg" means that the file will be automatically closed later
     reader = csv.DictReader(file)
-    # next(reader) # skip the header
-    # for row in reader:
-    #     print(row["language"])
-    scratch, c, python = 0, 0, 0
+    counts = {} # or: dict()
 
     for row in reader:
         favorite = row["language"]
-        if favorite == "Scratch":
-            scratch += 1
-        elif favorite == "C":
-            c += 1
-        elif favorite == "Python":
-            python += 1
+        if favorite in counts:
+            counts[favorite] += 1
+        else:
+            counts[favorite] = 1
 
-print(f"Scratch: {scratch}")
-print(f"C: {c}")
-print(f"Python: {python}")
+for favorite in counts: # for something in that dictionary
+    print(f"{favorite}: {counts[favorite]}")
+
+
+#     # next(reader) # skip the header
+#     # for row in reader:
+#     #     print(row["language"])
+
+#     scratch, c, python = 0, 0, 0
+
+#     for row in reader:
+#         favorite = row["language"]
+#         if favorite == "Scratch":
+#             scratch += 1
+#         elif favorite == "C":
+#             c += 1
+#         elif favorite == "Python":
+#             python += 1
+
+# print(f"Scratch: {scratch}")
+# print(f"C: {c}")
+# print(f"Python: {python}")
