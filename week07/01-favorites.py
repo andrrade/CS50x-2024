45285fdc2c789d441cfdcc39f55d7de6a0a301a2
Laura Andrade <devandrrade@gmail.com>
2024-10-15 13:23:59 +0000

    Tue, Oct 15, 2024, 10:23 AM -03:00

--- week07/01-favorites.py
+++ week07/01-favorites.py
@@ -5,7 +5,21 @@ import csv
 # close(file)
 
 with open("favorites.csv", "r") as file: # "witg" means that the file will be automatically closed later
-    reader = csv.reader(file)
+    reader = csv.DictReader(file)
     # next(reader) # skip the header
+    # for row in reader:
+    #     print(row["language"])
+    scratch, c, python = 0, 0, 0
+
     for row in reader:
-        print(row["language"])
+        favorite = row["language"]
+        if favorite == "Scratch":
+            scratch += 1
+        elif favorite == "C":
+            c += 1
+        elif favorite == "Python":
+            python += 1
+
+print(f"Scratch: {scratch}")
+print(f"C: {c}")
+print(f"Python: {python}")
