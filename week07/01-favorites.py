33913991f2a62c0764953a2e8c4a568c8886e6bc
Laura Andrade <devandrrade@gmail.com>
2024-10-15 17:26:12 +0000

    Tue, Oct 15, 2024, 2:26 PM -03:00

--- week07/01-favorites.py
+++ week07/01-favorites.py
@@ -24,7 +24,7 @@ with open("favorites.csv", "r") as file: # "witg" means that the file will be au
     #         counts[favorite] = 1
 
 # for favorite in sorted(counts, key=counts.get, reverse=True): # for something in that dictionary # get the value for that key
-for favorite, count in counts:
+for favorite, count in counts.most_common():
     print(f"{favorite}: {counts[favorite]}")
 
 
