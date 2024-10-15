0b03bfae11bb4209f8a6d42bfa68635f93ed38bf
Laura Andrade <devandrrade@gmail.com>
2024-10-15 13:08:51 +0000

    Tue, Oct 15, 2024, 10:08 AM -03:00

--- week07/01-favorites.py
+++ week07/01-favorites.py
@@ -6,4 +6,6 @@ import csv
 
 with open("favorites.csv", "r") as file: # "witg" means that the file will be automatically closed later
     reader = csv.reader(file)
-
+    next(reader)
+    for row in reader:
+        print(row[1])
