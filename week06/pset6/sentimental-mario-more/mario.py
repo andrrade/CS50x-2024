2f6642580c0b90e39b284863d2d8da14d4619850
Laura Andrade <devandrrade@gmail.com>
2024-10-17 13:37:58 +0000

    Thu, Oct 17, 2024, 10:37 AM -03:00

--- week06/pset6/sentimental-mario-more/mario.py
+++ week06/pset6/sentimental-mario-more/mario.py
@@ -6,11 +6,13 @@ while True:
         break
 
 for i in range(1, n + 1):
-    spaces = n - i
-    hashes = i
+    # Para a primeira pirâmide (à esquerda)
+    spaces = n - i  # Número de espaços à esquerda
+    hashes = i      # Número de hashes
 
-    print(" " * spaces, end="")
-    print("#" * hashes)
+    # Imprime a primeira pirâmide com os espaços
+    print(" " * spaces + "#" * hashes, end="  ")  # Adiciona 2 espaços entre as pirâmides
 
-for i in range(1, n + 1):  # De 1 a n
-    print("#" * i)  # Imprime i hashes
+    # Para a segunda pirâmide (à direita)
+    print("#" * i)  # Imprime a segunda pirâmide
+
