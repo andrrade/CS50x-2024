ad6d6ceed271526c03f9a9f07a347227e64106f1
Laura Andrade <devandrrade@gmail.com>
2024-10-17 13:24:41 +0000

    Thu, Oct 17, 2024, 10:24 AM -03:00

---
+++ week06/pset6/sentimental-mario-more/mario.py
@@ -0,0 +1,22 @@
+from cs50 import get_int
+
+while True:
+    n = get_int("Height: ")
+    if 1 <= n <= 8:
+        break
+
+for i in range(1, n + 1):
+    spaces = n - i
+    hashes = i
+
+    print(" " * spaces, end="")
+    print("#" * hashes)
+
+print()
+
+for i in range(1, n + 1):
+    spaces = n - i
+    hashes = i
+
+    print(" " * spaces, end="")
+    print("#" * hashes)
