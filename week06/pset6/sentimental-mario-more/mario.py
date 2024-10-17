b3b22f3b7a871cea427eb035de36dfca9ac565a5
Laura Andrade <devandrrade@gmail.com>
2024-10-17 13:30:27 +0000

    Thu, Oct 17, 2024, 10:30 AM -03:00

--- week06/pset6/sentimental-mario-more/mario.py
+++ week06/pset6/sentimental-mario-more/mario.py
@@ -1,18 +1,13 @@
 from cs50 import get_int
 
-# Solicita a altura da pirâmide até que seja um valor entre 1 e 8
 while True:
     n = get_int("Height: ")
     if 1 <= n <= 8:
         break
 
-# Laço para gerar as linhas das pirâmides
 for i in range(1, n + 1):
-    spaces = n - i  # Calcula os espaços para alinhar à direita
-    hashes = i  # Determina a quantidade de hashes
+    spaces = n - i
+    hashes = i
 
-    # Imprime os espaços e a primeira pirâmide
     print(" " * spaces, end="")
-    print("#" * hashes, end="  ")  # Adiciona dois espaços entre as pirâmides
-    # Imprime a segunda pirâmide sem espaços à frente
     print("#" * hashes)
