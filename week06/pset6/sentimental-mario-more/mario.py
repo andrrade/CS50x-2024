ee2edf9a15966ea5d1bd166986ae40bb1efa29a1
Laura Andrade <devandrrade@gmail.com>
2024-10-17 13:36:43 +0000

    Thu, Oct 17, 2024, 10:36 AM -03:00

--- week06/pset6/sentimental-mario-more/mario.py
+++ week06/pset6/sentimental-mario-more/mario.py
@@ -5,5 +5,13 @@ while True:
     if 1 <= n <= 8:
         break
 
+for i in range(1, n + 1):
+    spaces = n - i
+    hashes = i
+
+    print(" " * spaces, end="")
+    print("#" * hashes)
+
+print(end)
 for i in range(1, n + 1):  # De 1 a n
     print("#" * i)  # Imprime i hashes
