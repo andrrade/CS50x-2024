0347c1053f22eca7e6e419b3880313ba80101c6e
Laura Andrade <devandrrade@gmail.com>
2024-10-17 13:18:49 +0000

    Thu, Oct 17, 2024, 10:18 AM -03:00

--- week06/pset6/sentimental-mario-less/mario.py
+++ week06/pset6/sentimental-mario-less/mario.py
@@ -1,10 +1,15 @@
 from cs50 import get_int
 
+
 while True:
     n = get_int("Height: ")
+    width = n + 1
     if (n > 0):
         break
 
-for _ in range(n):
-    if ()
-    print("#" * n)
+for i in range(1, width):
+    hashes = i + 1
+    spaces = width - hashes
+
+    print(" " * spaces, end="")
+    print("#" * hashes)
