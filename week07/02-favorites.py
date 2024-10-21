0204c4562a0ae051245dbaedfa597d1e055b90ec
Laura Andrade <devandrrade@gmail.com>
2024-10-21 19:02:28 +0000

    Mon, Oct 21, 2024, 4:02 PM -03:00

--- week07/02-favorites.py
+++ week07/02-favorites.py
@@ -4,7 +4,7 @@ db = SQL("sqlite:///favorites.db")
 
 favorite = input("Favorite: ")
 
-rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?". favorite)
+rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?", favorite)
 row = rows[0]
 
 print(row["n"])
