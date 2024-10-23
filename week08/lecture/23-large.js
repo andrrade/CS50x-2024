cc6b18e8a043f8c145d6b64f33f00658e82360dc
Laura Andrade <devandrrade@gmail.com>
2024-10-23 18:06:46 +0000

    Wed, Oct 23, 2024, 3:06 PM -03:00

--- week08/lecture/23-large.js
+++ week08/lecture/23-large.js
@@ -1,4 +1,28 @@
 let WORDS = [
-    "aunt",
-
-]
+    "aunt", "apple", "animal", // A
+    "ball", "banana", "bridge", // B
+    "cat", "car", "candle", // C
+    "dog", "desk", "dance", // D
+    "elephant", "egg", "engine", // E
+    "flower", "fish", "friend", // F
+    "guitar", "game", "garden", // G
+    "hat", "house", "honey", // H
+    "ice", "island", "idea", // I
+    "juice", "jungle", "jacket", // J
+    "kite", "key", "kangaroo", // K
+    "lamp", "lion", "lake", // L
+    "moon", "mountain", "mouse", // M
+    "notebook", "night", "nest", // N
+    "ocean", "orange", "octopus", // O
+    "pizza", "pencil", "plant", // P
+    "queen", "quiz", "quiet", // Q
+    "rabbit", "rain", "river", // R
+    "sun", "star", "school", // S
+    "tree", "tiger", "train", // T
+    "umbrella", "uncle", "uniform", // U
+    "violin", "village", "volcano", // V
+    "water", "window", "whale", // W
+    "xylophone", "x-ray", "xenon", // X
+    "yellow", "yogurt", "yard", // Y
+    "zebra", "zoo", "zip" // Z
+];
