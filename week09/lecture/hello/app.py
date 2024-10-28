687bb91cd35d9d50c316b48c9aeb32e8d0bbf1e6
Laura Andrade <devandrrade@gmail.com>
2024-10-28 15:19:53 +0000

    Mon, Oct 28, 2024, 12:19 PM -03:00

--- week09/lecture/hello/app.py
+++ week09/lecture/hello/app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    name = request.args[""]
-    return render_template("01-index.html")
+    name = request.args["name"]
+    return render_template("01-index.html", placeholder=name)
