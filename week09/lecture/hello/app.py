d9cf132d040af669c2d1377560394af16343c565
Laura Andrade <devandrrade@gmail.com>
2024-10-29 20:30:52 +0000

    Tue, Oct 29, 2024, 5:30 PM -03:00

--- week09/lecture/hello/app.py
+++ week09/lecture/hello/app.py
@@ -12,12 +12,5 @@ def index():
 @app.route("/greet", methods=["POST"])
 def greet():
     name = request.form.get("name", "world")
-    return render_template("02-greet.html", name=name)
+    return render_template("01-greet.html", name=name)
 
-# @app.route("/", methods=["GET"], ["POST"])
-# def index():
-#     if request.method == "POST":
-#         name = request.form.get("name", "world")
-#         return render_template("02-greet.html", name=name)
-#     else:
-#         return render_template("01-index.html")
