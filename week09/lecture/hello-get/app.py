fa16f1f5ce2031d12d43bd1d88ef3907644ff77f
Laura Andrade <devandrrade@gmail.com>
2024-10-29 20:32:57 +0000

    Tue, Oct 29, 2024, 5:32 PM -03:00

--- week09/lecture/hello-get/app.py
+++ week09/lecture/hello-get/app.py
@@ -7,10 +7,10 @@ def index():
     # name = request.args.get("name", None)
     # name = request.args.get("name", "world")
     # return render_template("01-index.html", name=name) # name_of_paramter=value_of_parmter
-    return render_template("01-index.html")
+    return render_template("index.html")
 
 @app.route("/greet", methods=["POST"])
 def greet():
     name = request.form.get("name", "world")
-    return render_template("01-greet.html", name=name)
+    return render_template("greet.html", name=name)
 
