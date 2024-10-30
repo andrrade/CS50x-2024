0c6831e943bd7b81acf38d0ca072d5cd7069c22b
Laura Andrade <devandrrade@gmail.com>
2024-10-30 16:51:34 +0000

    Wed, Oct 30, 2024, 1:51 PM -03:00

--- week09/lecture/froshims/app.py
+++ week09/lecture/froshims/app.py
@@ -11,8 +11,9 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
+    # if not request.form.get("name") or not request.form.get("sport"):
         # return "failure"
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
     # return "success"
     return render_template("success.html")
